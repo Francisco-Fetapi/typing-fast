23d3d45510d2847cb3fa2f801449116d730c6a69
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-09-23 08:38:04 +0100

    theme dark defined like the one

--- src/core/mui.tsx
+++ src/core/mui.tsx
@@ -3,18 +3,15 @@ import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
 import CssBaseline from "@mui/material/CssBaseline";
 import { GlobalStyles } from "../styles/GlobalStyles";
-import { selectTheme } from "../store/App.selectors";
-import { useSelector } from "react-redux";
 
 interface Props {
   children: React.ReactElement;
 }
 
 export default function Layout({ children }: Props) {
-  const darkMode = useSelector(selectTheme);
   const theme = createTheme({
     palette: {
-      mode: darkMode ? "dark" : "light",
+      mode: "dark",
     },
   });
 
