08bc8829d7254d3f6ded0728bb7c19542642f588
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-09-25 07:24:31 +0100

    TimerBeforeStart added

--- src/components/TimerBeforeStart.tsx
+++ src/components/TimerBeforeStart.tsx
@@ -1,18 +1,36 @@
+import { useEffect, useState, useRef } from "react";
 import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
 import useBoolean from "../hooks/useBoolean";
+import { Text } from "../styles/General";
+
+const SECONDS_BEFORE_START = 3;
 
 export default function TimerBeforeStart() {
   const backdrop = useBoolean();
+  const [seconds, setSeconds] = useState(SECONDS_BEFORE_START);
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+
+  useEffect(() => {
+    backdrop.handleOpen();
+  }, []);
+  useEffect(() => {
+    if (seconds !== 0) {
+      timer.current = setTimeout(() => {
+        setSeconds(seconds - 1);
+      }, 1 * 1000);
+    } else {
+      clearTimeout(timer.current);
+      backdrop.handleClose();
+    }
+  }, [seconds]);
 
   return (
     <div>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={backdrop.open}
-        onClick={backdrop.handleClose}
       >
-        <CircularProgress color="inherit" />
+        <Text variant="h1">{seconds}</Text>
       </Backdrop>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import ProgressBar from "../components/ProgressBar";
+import TimerBeforeStart from "../components/TimerBeforeStart";
 import { AppContainer } from "../styles/General";
 
 export default function Home() {
   return (
     <AppContainer>
+      <TimerBeforeStart />
       <ProgressBar />
     </AppContainer>
   );
